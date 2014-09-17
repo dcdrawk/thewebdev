617e9f32582d7f5d269816862749bba00237daff
dcdrawk <dcdrawk@gmail.com>
2014-09-17 00:53:39 -0700

    Version 1.0.1 Now with projects stuff! Still need to add: more about me, 3 other projects, lightbox, php mailer, responsive

--- pub_html/js/scripts.js
+++ pub_html/js/scripts.js
@@ -1,49 +1,117 @@
-	/* 
-		This file builds on what we learned during the lecture/labs
-		It is meant to add a drop-down menu functionality for smart phones or small screens
-	*/
+/* 
+	This file builds on what we learned during the lecture/labs
+	It is meant to add a drop-down menu functionality for smart phones or small screens
+*/
 	
 //the function to hide the div, code is from: http://stackoverflow.com/questions/5277872/hide-a-div-if-screen-is-narrower-than-1024px */
 
 // At first I was having problems on mobile, the menu would collapse automatically on tapping after opening the menu, solved this by adding a boolean and changing how the menu opens and closes, this fixed the issue. Source: intuition
-var navShow = false;
 
-function hideDiv(){
-    if ($(window).width() > 624) { //width is slightly off for some reason, causes some problems across browsers
-		$("#menuBtn").hide();
-		$("#navBar").show();
-		$('#navBar').css('display', 'inline');
-    }else{
-		$("#menuBtn").show();
-		if(navShow == false){
-			$("#navBar").hide();
-		}else{
-		$("#navBar").show();
-		}
-    }
 
-}
 $(document).ready( function() {
+		
 	
-	$("#menuBtn").hide(); 	//hides the initial menu button, even though it's display is already set to none, allows us to show it again
-							//so if javascript is enabled, this button will show
-	
+	//hides the initial menu button, even though it's display is already set to none, allows us to show it again
+	//so if javascript is enabled, this button will show
     //on load
-    hideDiv();
 
     //on resize
+	
+	var esebc=false;
+	var scc=false;
+	var ufo=false;
+	var sandwich=false;
+	
+	
+	
     $(window).resize(function(){
-        hideDiv();
-
 	});
-	$("#menuBtn").click(function(){
-		$("#navBar").slideToggle();
-		if(navShow == false){
-			navShow = true;
+	
+	$(".esebc-toggle").click(function(){
+		$("#esebc-content").slideToggle();	
+		
+		if(esebc==false){
+			$(".esebc-expand").attr("src", "img/collapse.png");
+			esebc=true;
 		}else{
-			navShow = false;
+			esebc=false;
+			$(".esebc-expand").attr("src", "img/expand.png");
 		}
 	});
 	
-});
+	$(".esebc-collapse").click(function(){
+		$("#esebc-content").slideToggle();
+		$("html, body").animate({ scrollTop: $(".esebc-toggle").offset().top});
+		esebc=false;
+		$(".esebc-expand").attr("src", "img/expand.png");
+	});
+	
+	
+	
+	$(".scc-toggle").click(function(){
+		$("#scc-content").slideToggle();	
+		
+		if(scc==false){
+			$(".scc-expand").attr("src", "img/collapse.png");
+			scc=true;
+		}else{
+			scc=false;
+			$(".scc-expand").attr("src", "img/expand.png");
+		}
+	});
+	
+	$(".scc-collapse").click(function(){
+		$("#scc-content").slideToggle();
+		$("html, body").animate({ scrollTop: $(".scc-toggle").offset().top});
+		scc=false;
+		$(".scc-expand").attr("src", "img/expand.png");
+	});
+	
+	
+	
 
+	
+	
+	
+	$(".ufo-toggle").click(function(){
+		$("#ufo-content").slideToggle();	
+		
+		if(ufo==false){
+			$(".ufo-expand").attr("src", "img/collapse.png");
+			ufo=true;
+		}else{
+			ufo=false;
+			$(".ufo-expand").attr("src", "img/expand.png");
+		}
+	});
+	
+	$(".ufo-collapse").click(function(){
+		$("#ufo-content").slideToggle();
+		$("html, body").animate({ scrollTop: $(".ufo-toggle").offset().top});
+		ufo=false;
+		$(".ufo-expand").attr("src", "img/expand.png");
+	});
+	
+	
+	
+	$(".sandwich-toggle").click(function(){
+		$("#sandwich-content").slideToggle();	
+		
+		if(sandwich==false){
+			$(".sandwich-expand").attr("src", "img/collapse.png");
+			sandwich=true;
+		}else{
+			sandwich=false;
+			$(".sandwich-expand").attr("src", "img/expand.png");
+		}
+	});
+	
+	$(".sandwich-collapse").click(function(){
+		$("#sandwich-content").slideToggle();
+		$("html, body").animate({ scrollTop: $(".sandwich-toggle").offset().top});
+		sandwich=false;
+		$(".sandwich-expand").attr("src", "img/expand.png");
+	});
+	
+	
+});
